e7cf1cde79427dd8ba91993e2ef373bc019a46e2
Prasad Ravan <prasadcravan@gmail.com>
2020-09-27 21:22:59 +0530

    Added athelete service.

--- YoYoTest.DomainServices/AtheleteService/AtheleteService.cs
+++ YoYoTest.DomainServices/AtheleteService/AtheleteService.cs
@@ -1,10 +1,38 @@
 ﻿namespace YoYoTest.DomainServices.AtheleteService
 {
+    using System;
     using System.Collections.Generic;
     using Yo_Yo_Test.Models;
 
-    public class AtheleteService
+    /// <summary>
+    ///  Provides members to manage Athelete data entities.
+    /// </summary>
+    public class AtheleteService : IAtheleteService
     {
+        #region Member Variables
+
+        private bool isDisposed = false;
+
+        #endregion Member Variables
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes service with dependencies.
+        /// </summary>
+        public AtheleteService()
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Public Methods 
+
+        /// <summary>
+        /// Gets list of Atheletes.
+        /// </summary>
+        /// <returns>List of Athletes</returns>
         public List<Athelete> GetAtheletes()
         {
             List<Athelete> atheletes = new List<Athelete>();
@@ -22,5 +50,44 @@
 
             return atheletes;
         }
+
+        #endregion Public Methods
+
+        #region IDisposable Support
+
+        /// <summary>
+        /// Disposes the instance of UnitOfWork.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed state (managed objects).
+                    //if (unitOfWork != null)
+                    //    unitOfWork.Dispose();
+                }
+
+                // free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // set large fields to null.
+
+                isDisposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Disposes current instance of the service.
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion IDisposable Support
     }
 }
--- YoYoTest.DomainServices/AtheleteService/IAtheleteService.cs
+++ YoYoTest.DomainServices/AtheleteService/IAtheleteService.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace YoYoTest.DomainServices.AtheleteService
+﻿namespace YoYoTest.DomainServices.AtheleteService
 {
-    public interface IAtheleteService
-    {
+    using System;
+    using System.Collections.Generic;
+    using Yo_Yo_Test.Models;
 
+    /// <summary>
+    /// Exposes members to manage Athletes data entities.
+    /// </summary>
+    public interface IAtheleteService : IDisposable
+    {
+        /// <summary>
+        /// Gets list of Atheletes.
+        /// </summary>
+        /// <returns>List of Athletes</returns>
+        List<Athelete> GetAtheletes();
     }
 }
